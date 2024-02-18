ab086f606e07fa0352c6d3a6703d16ca72cdee37
David Morales <morales.david1997@gmail.com>
2024-02-17 23:36:57 -0400

    refactor: update Flight struct fields and types

--- flight/model.go
+++ flight/model.go
@@ -1,24 +1,25 @@
 package flight
 
 type Flight struct {
-	Hex            string  `json:"hex"`
-	RegisterNumber string  `json:"reg_number"`
-	AircraftICAO   string  `json:"aircraft_icao"`
-	Flag           string  `json:"flag"`
-	Latitude       float64 `json:"lat,string"`
-	Longitude      float64 `json:"lgn,string"`
-	Altitude       int64   `json:"alt,string"`
-	Direction      int64   `json:"dir,string"`
-	Speed          int64   `json:"speed,string"`
-	Squawk         string  `json:"squawk"`
-	AirlineICAO    string  `json:"airline_icao"`
-	AirlineIATA    string  `json:"airline_iata"`
-	FlightNumber   string  `json:"flight_number"`
-	FlightICAO     string  `json:"flight_icao"`
-	FlightIATA     string  `json:"flight_iata"`
-	Duration       string  `json:"duration"`
-	Updated        int64   `json:"updated,string"`
-	Status         string  `json:"status"`
+	Hex                string  `json:"hex"`
+	RegistrationNumber string  `json:"reg_number"`
+	AircraftICAO       string  `json:"aircraft_icao"`
+	Flag               string  `json:"flag"`
+	Latitude           float64 `json:"lat,string"`
+	Longitude          float64 `json:"lgn,string"`
+	Altitude           int64   `json:"alt,string"`
+	Direction          int64   `json:"dir,string"`
+	Speed              int64   `json:"speed,string"`
+	Velocity           int64   `json:"v_speed,string"`
+	Squawk             string  `json:"squawk"`
+	AirlineICAO        string  `json:"airline_icao"`
+	AirlineIATA        string  `json:"airline_iata"`
+	FlightNumber       string  `json:"flight_number"`
+	FlightICAO         string  `json:"flight_icao"`
+	FlightIATA         string  `json:"flight_iata"`
+	Duration           int64   `json:"duration,string"`
+	Updated            int64   `json:"updated,string"`
+	Status             string  `json:"status"`
 }
 
 func (f *Flight) IsInBoundingBox(southWestLatitude, southWestLongitude, northEastLatitude, northEastLongitude float64) bool {
