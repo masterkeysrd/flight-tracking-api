92a921f35a109ce6a92699607dfcaba4ddd890e7
David Morales <morales.david1997@gmail.com>
2024-02-17 21:49:03 -0400

    feat(flight): Add flight data file handling to repository

--- flight/repository.go
+++ flight/repository.go
@@ -2,8 +2,9 @@ package flight
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
-	"time"
+	"os"
 )
 
 type BoundingBox struct {
@@ -14,18 +15,18 @@ type BoundingBox struct {
 }
 
 type FlightFilter struct {
-	BoundingBox *BoundingBox
-	Zoom		int64
-	Hex         string
+	BoundingBox    *BoundingBox
+	Zoom           int64
+	Hex            string
 	RegisterNumber string
-	AirlineICAO string
-	AirlineIATA string
-	Flag         string
-	FlightICAO    string
-	FlightIATA   string
+	AirlineICAO    string
+	AirlineIATA    string
+	Flag           string
+	FlightICAO     string
+	FlightIATA     string
 }
 
-type interface Repository {
+type Repository interface {
 	// GetMany returns a list of parameters
 	GetMany(ctx context.Context, flightFilter *FlightFilter) ([]*Flight, error)
 }
@@ -34,8 +35,36 @@ type repository struct {
 	flights []*Flight
 }
 
-func NewRepository() Repository {
-	return &repository{
-		flights: make([]*Flight, 0),
+func (r *repository) GetMany(ctx context.Context, flightFilter *FlightFilter) ([]*Flight, error) {
+	var flights []*Flight
+	for _, f := range r.flights {
+		if flightFilter.BoundingBox != nil && !ByBoundingBox(flightFilter, f) {
+			continue
+		}
+		flights = append(flights, f)
 	}
+	return flights, nil
+}
+
+func NewRepository(config *Config) (Repository, error) {
+	filename := config.Filename()
+
+	if filename == "" {
+		return nil, fmt.Errorf("flight data file not found")
+	}
+
+	file, err := os.Open(filename)
+	defer file.Close()
+	if err != nil {
+		return nil, fmt.Errorf("error opening flight data file: %v", err)
+	}
+
+	var flights []*Flight
+	if err := json.NewDecoder(file).Decode(&flights); err != nil {
+		return nil, fmt.Errorf("error decoding flight data: %v", err)
+	}
+
+	return &repository{
+		flights: flights,
+	}, nil
 }
