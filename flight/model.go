e9aed44dcd57a8660437961a8d25e49d024f0f72
David Morales <morales.david1997@gmail.com>
2024-02-17 21:47:59 -0400

    fix(flight): update Flight struct fields to use appropriate data types

--- flight/model.go
+++ flight/model.go
@@ -1,24 +1,24 @@
 package flight
 
 type Flight struct {
-	Hex            string `json:"hex"`
-	RegisterNumber string `json:"reg_number"`
-	AircraftICAO   string `json:"aircraft_icao"`
-	Flag           string `json:"flag"`
-	Latitude       float  `json:"lat"`
-	Longitude      float  `json:"lgn"`
-	Altitude       int64  `json:"alt"`
-	Direction      int64  `json:"dir"`
-	Speed          int64  `json:"speed"`
-	Squawk         string `json:"squawk"`
-	AirlineICAO    string `json:"airline_icao"`
-	AirlineIATA    string `json:"airline_iata"`
-	FlightNumber   string `json:"flight_number"`
-	FlightICAO     string `json:"flight_icao"`
-	FlightIATA     string `json:"flight_iata"`
-	Duration       string `json:"duration"`
-	Updated        int64  `json:"updated"`
-	Status         string `json:"status"`
+	Hex            string  `json:"hex"`
+	RegisterNumber string  `json:"reg_number"`
+	AircraftICAO   string  `json:"aircraft_icao"`
+	Flag           string  `json:"flag"`
+	Latitude       float64 `json:"lat,string"`
+	Longitude      float64 `json:"lgn,string"`
+	Altitude       int64   `json:"alt,string"`
+	Direction      int64   `json:"dir,string"`
+	Speed          int64   `json:"speed,string"`
+	Squawk         string  `json:"squawk"`
+	AirlineICAO    string  `json:"airline_icao"`
+	AirlineIATA    string  `json:"airline_iata"`
+	FlightNumber   string  `json:"flight_number"`
+	FlightICAO     string  `json:"flight_icao"`
+	FlightIATA     string  `json:"flight_iata"`
+	Duration       string  `json:"duration"`
+	Updated        int64   `json:"updated,string"`
+	Status         string  `json:"status"`
 }
 
 func (f *Flight) IsInBoundingBox(southWestLatitude, southWestLongitude, northEastLatitude, northEastLongitude float64) bool {
